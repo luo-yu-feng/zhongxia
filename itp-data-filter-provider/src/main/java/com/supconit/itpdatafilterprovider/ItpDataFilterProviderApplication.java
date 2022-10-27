8cd5f9d2b341a457bb52bd236b0d754d4b68c30c
liuyunfei <liuyunfei@exiot.cmcc>
2022-10-27 17:30:20 +0800

    lyf-改动-master

---
+++ itp-data-filter-provider/src/main/java/com/supconit/itpdatafilterprovider/ItpDataFilterProviderApplication.java
@@ -0,0 +1,13 @@
+package com.supconit.itpdatafilterprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItpDataFilterProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItpDataFilterProviderApplication.class, args);
+  }
+
+}
---
+++ itp-data-filter-provider/src/test/java/com/supconit/itpdatafilterprovider/ItpDataFilterProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supconit.itpdatafilterprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItpDataFilterProviderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
